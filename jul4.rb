ec395a17d4a987e9b5f8dc3cf8c7d709935cc444
shaanray <shaanrayoffice@gmail.com>
2017-07-04 09:08:23 -0400

    Added today's questions

---
+++ jul4.rb
@@ -0,0 +1,20 @@
+# Create the following array:
+# [1, 12, 144, 1728, 20736, 248832, 2985984, 35831808]
+# Notice that every element of the array is equal to the previous element times twelve. (Finding out how to do exponentials might be helpful here)
+
+
+
+
+# Reversals
+# Permanently change this array
+# words = ['first', 'second', 'third', 'fourth', 'fifth', 'sixth']
+# to this one
+# words
+# => ["htxis", "htfif", "htruof", "driht", "dnoces", "tsrif"]
+
+
+
+# Array to Hash
+# movies = [['Alfonso Cuaron', 'Gravity'], ['Spike Jonze', 'Her'], ['Martin Scorsese', 'The Wolf of Wall Street']]
+# Use the "movies" array to construct the following hash:
+# {"Alfonso Cuaron" => "Gravity", "Spike Jonze" => "Her", "Martin Scorsese" => "The Wolf of Wall Street"}
