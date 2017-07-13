5855a40f58ec99f2c9e029b4efa4a20702d934b3
shaanray <shaanrayoffice@gmail.com>
2017-07-13 09:20:46 -0400

    Initializing today's problem

---
+++ jul13.rb
@@ -0,0 +1,26 @@
+# ====================>> Assignment
+# You may have heard that computers represent all data with binary numbers.
+# In the binary number system, only two digits are used: 0 and 1. Contrast this to the decimal number system we are used to, which uses 10 digits: 0 through 9.
+# In the decimal number system, when the least significant number is 9, and you +1 to that number, then the least significant number resets back to 0, and a 1 is added to the front of the number, producing 10. The same thing happens when you have 99, and you +1 to it. Then all the 9's reset to 0, and a 1 is added to the front of the number, producing 100.
+# It works similarly in binary, except it will reset much quicker:
+# 0        = 0
+# 1        = 1
+# 10       = 2
+# 11       = 3
+# 100      = 4
+# 101      = 5
+# 110      = 6
+# 111      = 7
+# 1000     = 8
+# ...
+# 11111111 = 255
+# Write a method that can covert a string representing a binary number to a decimal number. You may assume that the string will only contain 0's and 1's.
+# bin_to_dec('10')     --> 2
+# bin_to_dec('101')    --> 5
+# bin_to_dec('101100') --> 44
+
+def convert_to_dec(binary_input)
+
+
+
+end
