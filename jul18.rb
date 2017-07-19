32c83067f12b1ff1b73dff4b383b4dae0adb682e
shaanray <shaanrayoffice@gmail.com>
2017-07-18 20:30:19 -0400

    starting second question

--- jul18.rb
+++ jul18.rb
@@ -74,3 +74,37 @@ digits = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
 puts create_phone_number(digits)
 
 puts refactored_create_phone_number(digits)
+
+
+
+
+
+
+# ======================================================
+
+
+# Luck check
+#
+# In some countries of the former Soviet Union there was a belief about lucky tickets.
+# A transport ticket of any sort was believed to possess luck if sum of digits on the
+# left half of its number was equal to the sum of digits on the right half. Here are
+# examples of such numbers:
+#
+# 003111 # 3 = 1 + 1 + 1
+# 813372 # 8 + 1 + 3 = 3 + 7 + 2
+# 17935 # 1 + 7 = 3 + 5
+# 56328116
+# Such tickets were either eaten after being used or collected for bragging rights.
+# Your task is to write a method luck_check(str), which returns true if argument is
+# string decimal representation of a lucky ticket number, or false for all other numbers.
+# It should handle errors for empty strings or strings which don't represent a decimal number.
+
+str = [8,4,8]
+
+def luck_check(str)
+  p str.count
+
+  p str
+end
+
+luck_check(str)
